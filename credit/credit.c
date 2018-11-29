02295082e8a5132646d2d9a972f2af8eaf323ad7
X <hrochin001@gmail.com>
2018-11-29 13:59:29 -0800

    Refactored code

--- credit/credit.c
+++ credit/credit.c
@@ -3,9 +3,8 @@
 #include <math.h>
 
 int digit_count(long long);
-int initial_sum(long long, int, bool);
-int sum_remaining(long long, int, bool);
-void get_type(long long, int);
+void get_type(long long);
+bool check_sum(long long);
 
 int main(void){
 
@@ -14,26 +13,13 @@ int main(void){
     printf("Number: ");
     scanf("%lld", &credit_card);
 
-    printf("Your number is: %lld\n", credit_card);
-    int count = digit_count(credit_card);
-    //printf("You have %d numbers.\n", count);
-    bool is_even = (count % 2) + 1;
-    int first_sum = initial_sum(credit_card, count, is_even);
-    int remaining_sum = sum_remaining(credit_card, count, is_even);
-
-    printf("Your initial sum is: %d\n", first_sum);
-    printf("remaining sum is: %d\n", remaining_sum);
-
-    if((first_sum + remaining_sum)%10 == 0){
+    if(check_sum(credit_card)){
         printf("Valid\n");
     }
     else{
-        printf("inValid\n");
+        printf("Invalid\n");
     }
-
-    get_type(credit_card, count);
-
-
+    get_type(credit_card);
     return 0;
 }
 
@@ -47,133 +33,58 @@ int digit_count(long long number){
     return count;
 }
 
-int initial_sum(long long number, int count, bool is_even){
-
-    int loop = count/2;
-    long mod = 10;
-    long int_div = 1;
-
-    int sum = 0;
-
-    printf("\n-------------------\n");
-
-    if(is_even){
-        mod = 100;
-        int_div = 10;
-        while(loop > 0){
-            int add = (int)((number % mod) / int_div) * 2;
-            if(add >= 10){
-                add = 1 + (add % 10);
-            }
-            printf("adding: %d\n", add);
-            sum += add;
-            mod *= 100;
-            int_div *= 100;
-            loop--;
-        }
-    }
-    else{
-        while(loop > 0){
-            int add = (int)((number % mod) / int_div) * 2;
-            if(add >= 10){
-                add = 1 + (add % 10);
-            }
-            printf("adding: %d\n", add);
-            sum += add;
-            mod *= 100;
-            int_div *= 100;
-            loop--;
-        }
-    }
-
-    return sum;
-}
-
-int sum_remaining(long long number, int count, bool is_even){
-
-    int loop = count/2;
-    long mod = 10;
-    long int_div = 1;
-
-    int sum = 0;
-
-    printf("\n-------------------\n");
-
-    if(!is_even){
-        mod = 100;
-        int_div = 10;
-        while(loop > 0){
-            int add = (int)((number % mod) / int_div);
-            printf("adding: %d\n", add);
-            sum += add;
-            mod *= 100;
-            int_div *= 100;
-            loop--;
-        }
-    }
-    else{
-        while(loop > 0){
-            int add = (int)((number % mod) / int_div);
-            printf("adding: %d\n", add);
-            sum += add;
-            mod *= 100;
-            int_div *= 100;
-            loop--;
-        }
-    }
-
-    return sum;
-}
-
-void get_type(long long number, int count){
+void get_type(long long number){
     // check type
-    long long int_div = pow(10,count-2);
+    int count = digit_count(number);
+    long long int_div = pow(10, count - 2);
     int type = (int)(number/int_div);
 
-    printf("\n-------------------\n");
-    printf("type: %d\n, int_div: %lld\n", type, int_div);
-    printf("\n-------------------\n");
-
-    switch(type){
-        case 34:
-            if(count == 15){
-                printf("Amex\n");
-            }
-            break;
-        case 37:
-            if(count == 15){
-                printf("Amex\n");
-            }
-            break;
-        case 51:
-            if(count == 16){
-                printf("MasterCard\n");
-            }
-            break;
-        case 52:
-            if(count == 16){
-                printf("MasterCard\n");
-            }
-            break;
-        case 53:
-            if(count == 16){
-                printf("MasterCard\n");
-            }
-            break;
-        case 54:
-            if(count == 16){
-                printf("MasterCard\n");
-            }
-            break;
-        case 55:
-            if(count == 16){
-                printf("MasterCard\n");
-            }
-            break;
-        case 4:
-            if(count == 16 || count == 13){
-                printf("Visa\n");
-            }
-            break;
+    if((type == 34 || type == 37) && count == 15){
+        printf("Amex\n");
     }
+    else if(type >= 51 && type <= 55 && count == 16){
+        printf("MasterCard\n");
+    }
+    else if( (type >= 40 && type <= 49) && (count == 13 || count == 16)){
+        printf("Visa\n");
+    }
+    else{
+        printf("Unknown Vendor\n");
+    }
+}
+
+bool check_sum(long long number){
+
+    long mod = 10;
+    long int_div = 1;
+    int count = digit_count(number);
+    int loop = 1;
+    int sum = 0;
+
+    while(loop <= count){
+        // run through each digit from least significant
+        // and add according to is_even
+
+        // Check if odd index
+        if( loop%2 != 0){
+            sum += (int)((number % mod) / int_div);
+            //printf("adding: %d\n", add_val);
+            //sum += add_val;
+        }
+        else{
+            int add_val = (int)((number % mod) / int_div) * 2;
+            //printf("adding: %d\n", add_val);
+            if(add_val >= 10){
+                sum+= 1 + (add_val % 10);
+            }
+            else{
+                sum+= add_val;
+            }
+        }
+    mod *= 10;
+    int_div *= 10;
+    loop++;
+    }
+    //printf("%d\n", sum);
+    return (sum % 10 == 0) ? true: false;
 }
